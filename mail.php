37fde3ff5c8d4e810a21ddf65308d04984cb0a69
Alex <9898584868z@gmail.com>
2018-09-09 01:32:47 +0300

    update content, php, added new timer

--- mail.php
+++ mail.php
@@ -5,8 +5,9 @@ $sitename = "bitcoiner";
 
 $name = trim($_POST["name"]);
 $phone = trim($_POST["phone"]);
-$text = trim($_POST["text"]);
-$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
+// $text = trim($_POST["text"]);
+$number = trim($_POST["number"]);
+$message = "Имя: $name \nТелефон: $phone \nМонет: $number";
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,5 @@
 //= ../../node_modules/jquery/dist/jquery.min.js
-//= ../../node_modules/jquery.countdown/jquery.countdown.js
-
-    //Timer
-    var fiveSeconds = new Date().getTime() + 15500000;
-    $('#timer').countdown(fiveSeconds, {elapse: true})
-    .on('update.countdown', function(event) {
-        var $this = $(this);
-        if (event.elapsed) {
-            $this.html(
-            window.open('index.html', '_self')
-        );
-        } else {
-            $this.html(event.strftime('%H:%M:%S'));
-        }
-    });
-
 $(document).ready(function() {
-
-
     //Form
     $("#form").submit(function() {
         $.ajax({
@@ -26,7 +8,7 @@ $(document).ready(function() {
             data: $(this).serialize()
         }).done(function() {
             $(this).find("input").val("");
-            alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+            alert("Спасибо за заявку!");
             $("#form").trigger("reset");
         });
         return false;
