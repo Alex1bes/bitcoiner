f431bc440e0d708732b668dbe7e324c99ad95023
Alex <9898584868z@gmail.com>
2018-09-09 22:17:49 +0300

    upload form

--- mail.php
+++ mail.php
@@ -1,13 +1,18 @@
 <?php
 
-$recepient = "alexbesinfo@gmail.com";
+$recepient = "alexbesinfo@gmail.com, 9898584868z@gmail.com";
 $sitename = "bitcoiner";
 
 $name = trim($_POST["name"]);
 $phone = trim($_POST["phone"]);
-// $text = trim($_POST["text"]);
 $number = trim($_POST["number"]);
-$message = "Имя: $name \nТелефон: $phone \nМонет: $number";
+$address = trim($_POST["address"]);
+$message = "
+    Имя: $name \n
+    Телефон: $phone \n
+    Монет: $number \n
+    Адрес доставки: $address
+    ";
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
